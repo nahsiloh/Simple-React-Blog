11060febff21e5561a285ab9df39eb74c90d9224
nahsiloh <lishan1512@gmail.com>
2019-09-13 18:09:22 +0800

    minor changes

--- blog.js
+++ blog.js
@@ -3,7 +3,6 @@ const root = document.querySelector("#blog");
 function Post(prop) {
   return (
     <div>
-      <h1>My Blog Post</h1>
       <article>
         <h2>{prop.title}</h2>
         <p>Written by: {prop.author}</p>
@@ -16,6 +15,7 @@ function Post(prop) {
 class Blog extends React.Component {
   render() {
     return (
+      <h1>My Blog Post</h1>
       <div>
         <Post title="Blog One" author="Thing 1" date="13 Sep 2019" />
         <Post title="Blog Two" author="Thing 2" date="13 Sep 2019" />
