8279871d18b500f41bbb809949d5b8954f3b80d2
nahsiloh <lishan1512@gmail.com>
2019-09-13 18:00:02 +0800

    first commit

---
+++ blog.js
@@ -0,0 +1,28 @@
+const root = document.querySelector("#blog");
+
+function Post(prop) {
+  return (
+    <div>
+      <h1>My Blog Post</h1>
+      <article>
+        <h2>{prop.title}</h2>
+        <p>Written by: {prop.author}</p>
+        <p>{prop.date}</p>
+      </article>
+    </div>
+  );
+}
+
+class Blog extends React.Component {
+  render() {
+    return (
+      <div>
+        <Post title="Blog One" author="Thing 1" date="13 Sep 2019" />
+        <Post title="Blog Two" author="Thing 2" date="13 Sep 2019" />
+        <Post title="Blog Three" author="Thing 3" date="13 Sep 2019" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Blog />, root);
